ffa3638e540bc1363a51fafa55e2b748c43926ff
Anshu <anshusharma1983@gmail.com>
2020-12-10 13:02:30 +0530

    Currying

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import { compose, pipe } from "lodash/fp";
 
 let input = "   Javascript  ";
-let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div>${str}</div>`;
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`; // currying
 const toLowerCase = (str) => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
-
-const transform = pipe(trim, toLowerCase, wrapInDiv);
+const transform = pipe(trim, toLowerCase, wrap("div"));
 const transformedOutput = transform(input);
 
 console.log(transformedOutput);
