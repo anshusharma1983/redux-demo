0f818a313fcb97803c7841191610285aa7c56f81
Anshu <anshusharma1983@gmail.com>
2020-12-12 13:01:15 +0530

    Action creators

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import store from "./store";
+import * as actions from "./actionTypes";
+import { bugAdded } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch({
-  type: "bugAdded",
-  payload: {
-    description: "bug1",
-  },
-});
+store.dispatch(bugAdded("bug1"));
 
 unsubscribe();
 
 store.dispatch({
-  type: "bugRemoved",
+  type: actions.BUG_REMOVED,
   payload: {
     id: 1,
   },
